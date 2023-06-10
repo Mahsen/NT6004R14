ac5a888fd18d61f0faa0342d9d2fc805e62e9837
Mohammad Lotfi <not.select@gmail.com>
2023-06-10 14:19:31 +0330

    Add lcd

---
+++ Application/Driver/lcd.hpp
@@ -0,0 +1,14 @@
+#ifndef __LCD_HPP
+#define __LCD_HPP
+#ifdef __cplusplus
+extern "C" {
+#endif
+//----------------------------------------------------------
+#include "defines.h"
+//----------------------------------------------------------
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
